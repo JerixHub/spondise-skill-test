33d2a33ed0a1c6c66773c715adb5a65feb771e51
jerixhub <carillojericjohn@gmail.com>
2023-04-06 01:18:37 +0800

    added default pages

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,4 +1,28 @@
 <template>
-<Nuxt />
+<div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand to="/">Home</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+        <b-nav-item href="#" @click.prevent="logout()">Sign Out</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  <Nuxt />
+</div>
+
 
 </template>
+
+<script>
+export default{
+    methods: {
+        async logout(){
+            this.$auth.logout()
+        }
+    }
+}
+</script>
---
+++ client/pages/register.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <b-card>
+        <b-form @submit="onSubmit">
+            <b-form-group
+                id="input-group-1"
+                label="Name:"
+                label-for="input-1"
+            >
+                <b-form-input
+                id="input-1"
+                v-model="form.name"
+                type="text"
+                placeholder="Enter Name"
+                required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Email Address:" label-for="input-2">
+                <b-form-input
+                id="input-2"
+                v-model="form.email"
+                placeholder="Enter email"
+                required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Password:" label-for="input-3">
+                <b-form-input
+                id="input-3"
+                v-model="form.password"
+                type="password"
+                placeholder="Enter password"
+                required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-button type="submit" variant="primary">Submit</b-button>
+        </b-form>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+    auth: false,
+
+    data(){
+        return{
+            form: {
+                name: '',
+                email: '',
+                password: ''
+            }
+        }
+    },
+
+    methods: {
+        onSubmit(){
+            this.$axios.post('http://localhost:8000/api/register', {
+                name: form.name,
+                email: form.email,
+                password: form.password
+            })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
+        }
+    }
+}
+</script>
